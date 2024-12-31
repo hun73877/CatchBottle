07fac301169b7f9556fa849a7ae2677062341b77
winter <winter.kim@mvlchain.io>
2024-12-31 17:26:36 +0900

    Update application ID to "dev.love.winter.catchbottle"

--- app/src/main/java/dev/love/winter/catchbottle/activity/splash/SplashActivity.kt
+++ app/src/main/java/dev/love/winter/catchbottle/activity/splash/SplashActivity.kt
@@ -1,16 +1,18 @@
-package com.season.winter.catchbottle.activity.splash.compose
+package dev.love.winter.catchbottle.activity.splash
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 @SuppressLint("CustomSplashScreen")
-class SplashActivity : ComponentActivity() {
+class SplashActivity: AppCompatActivity() {
 
 //    private val viewModel: SplashViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
